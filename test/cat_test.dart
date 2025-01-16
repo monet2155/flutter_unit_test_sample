caa1aebf3cb1d2c6dd305b04b52f49ba90b15374
monet2155 <monet9812@gmail.com>
2025-01-16 16:40:26 +0900

    feat: init cat.sound test code

--- test/cat_test.dart
+++ test/cat_test.dart
@@ -1,10 +1,20 @@
 import 'package:flutter_unit_test_sample/cat.dart';
 import 'package:mockito/annotations.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 
 // Annotation which generates the cat.mocks.dart library and the MockCat class.
 @GenerateNiceMocks([MockSpec<Cat>()])
 import 'cat_test.mocks.dart';
 
 void main() {
-  var cat = MockCat();
+  group('cat', () {
+    test('cat test', () {
+      var cat = MockCat();
+
+      cat.sound();
+
+      verify(cat.sound());
+    });
+  });
 }
